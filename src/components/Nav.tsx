5918991aa5267d90c607102f43f43296be912481
xcorp-llc <support@xcorpllc.com>
2023-11-17 17:16:09 -0500

    accessibility 2

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -29,23 +29,29 @@ export default function Nav() {
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
               className="fixed top-0 right-0 h-screen w-screen grid place-content-center gap-20 px-8 md:px-28 py-[7rem] backdrop-blur-lg font-thin text-center text-4xl">
-              <a href="/" className="text-slate-800 active:text-slate-700">
+              <a
+                href="/"
+                className="text-slate-800 active:text-slate-700"
+                title="Home">
                 ./
               </a>
               <a
                 onClick={() => setNavOpen(false)}
                 href="#contact"
+                title="Contact me"
                 className="text-slate-800 active:text-slate-700">
                 Contact
               </a>
               <a
                 href="https://github.com/camc8/"
+                title="My Github"
                 target="_blank"
                 className="text-slate-800 active:text-slate-700">
                 Github
               </a>
               <a
                 href="https://www.linkedin.com/in/cameron-clark-6b13281b2"
+                title="My LinkedIn"
                 target="_blank"
                 className="text-slate-800 active:text-slate-700">
                 LinkedIn
